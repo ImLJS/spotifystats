e811631ac44878273159555e09ab5655ab75f944
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-12-06 09:26:38 +0530

    Created Branch to Test UI Changes Only

---
+++ components/auth/user-auth-form.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export function UserAuthForm({ className, ...props }) {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <Button variant="outline" type="button" disabled={isLoading}>
+            {isLoading ? (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <Icons.gitHub className="mr-2 h-4 w-4" />
+            )}{' '}
+            GitHub
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
