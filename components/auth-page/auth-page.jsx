9eb0c36876b8fd3906b5f42c41a9e267b46559f9
muniraju.a <muniraju.a@lge.com>
2024-12-10 09:59:07 +0530

    Fix Responsive Behaviour

--- components/auth-page/auth-page.jsx
+++ components/auth-page/auth-page.jsx
@@ -22,30 +22,39 @@ const AuthPage = () => {
       <main className="flex flex-grow flex-col items-center justify-center p-4">
         <div className="w-full max-w-6xl">
           {/* Content Section */}
-          <div className="flex flex-col items-center md:flex-col lg:flex-row">
-            <div className="text-center md:text-center lg:w-1/2 lg:text-left lg:pr-12">
-              <h1 className="mb-4 text-4xl font-bold lg:text-5xl">Spotify Stats</h1>
+          <div className="flex flex-col items-center 
+            md:flex-col 
+            xl:flex-row 
+            space-y-8 
+            xl:space-y-0 
+            xl:space-x-12">
+            <div className="w-full text-center 
+              xl:w-1/2 
+              xl:text-left">
+              <h1 className="mb-4 text-4xl font-bold xl:text-5xl">
+                Spotify Stats
+              </h1>
               <p className="mb-8 text-xl text-gray-600">
                 Dive deep into your music listening habits, discover your top
                 tracks, artists, and get personalized insights about your Spotify
                 journey.
               </p>
-              <div className="flex justify-center space-x-4 md:justify-center lg:justify-start">
+              <div className="flex justify-center space-x-4 
+                xl:justify-start">
                 <Button size="lg">Sign In</Button>
-                <Button variant="outline" size="lg" className="hidden lg:inline-flex">
-                  Learn More
-                </Button>
               </div>
             </div>
 
             {/* Image Section */}
-            <div className="mt-8 w-full md:mt-12 lg:mt-0 lg:w-1/2">
+            <div className="w-full 
+              md:max-w-[500px] 
+              xl:w-1/2">
               <Image
                 src="/music.svg"
                 height={500}
                 width={500}
                 alt="Music"
-                className="mx-auto h-auto w-full max-w-[400px] lg:max-w-[500px]"
+                className="mx-auto h-auto w-full"
               />
             </div>
           </div>
