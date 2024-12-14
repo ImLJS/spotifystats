dd15d844b4658ff8a964d70b374baa1494170a2e
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2024-12-14 11:14:12 +0530

    [Feature] : Update Dashboard

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import { HeroSection } from '@/components/hero/hero-section';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
+import Nav from '@/components/navbar/nav';
 
 async function HomePage() {
   const session = await auth();
@@ -10,10 +11,13 @@ async function HomePage() {
   }
   
   return (
-    <main className="flex flex-grow flex-col items-center justify-center p-4">
+    <main className='flex min-h-screen flex-col'>
+      <Nav/>
+    <div className="flex flex-grow flex-col items-center justify-center p-4">
       <div className="w-full max-w-6xl">
         <HeroSection />
       </div>
+    </div>
     </main>
   );
 }
